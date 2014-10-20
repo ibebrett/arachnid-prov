c5c6af54b765476231af91f869392bb48aca515a
Brett Jurman <i.be.brett@gmail.com>
2014-10-20 04:11:06 +0000

    added autoscaling group

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -17,10 +17,28 @@ resource "aws_security_group" "scraper" {
 }
 
 resource "aws_instance" "scraper" {
-    ami = "ami-bc9015d4"
+    ami = "ami-96981dfe"
     instance_type = "t1.micro"
     key_name = "brettaws"
-    count = 1
+    security_groups = ["${aws_security_group.scraper.name}"]
+    count = 3
+}
+
+resource "aws_launch_configuration" "zookeeper" {
+    name = "zookeeper"
+    image_id = "ami-14aa2f7c"
+    instance_type = "t1.micro"
+    key_name = "brettaws"
     security_groups = ["${aws_security_group.scraper.name}"]
 }
 
+resource "aws_autoscaling_group" "zookeeper" {
+    availability_zones = ["us-east-1a"]
+    name = "zookeeper"
+    max_size = 3
+    min_size = 3
+    health_check_grace_period = 300
+    desired_capacity = 3
+    force_delete = true
+    launch_configuration = "${aws_launch_configuration.zookeeper.name}"
+}
