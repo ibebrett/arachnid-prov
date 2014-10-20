845b8fec85df7062cbb4b5a58dfabce4160c5ecb
Ubuntu <ubuntu@ip-172-31-12-199.ec2.internal>
2014-10-20 02:26:52 +0000

    modified provisioning script

--- roles/scraper/provision.sh
+++ roles/scraper/provision.sh
@@ -2,20 +2,27 @@ sleep 30 # sleep so that the provisioner can install
 
 USER_HOME=/home/ubuntu
 
+# install apt dependencies
 sudo apt-get update
 sudo apt-get install -y \
-    libpq-dev \
+    libpq-dev  \
     python-dev \
-    git
+    python-pip \
+    git \
+    supervisor
 
-# check if file exists and move file
+# install supervisor confs
+sudo cp /tmp/scraper.conf /etc/supervisor/conf.d/scraper.conf
+
+# move tools
 mkdir -p $USER_HOME/tools/
 tar --directory $USER_HOME/tools/ -xvf /tmp/phantomjs.tar.bz2
 
 # clone the arachnid repo
-git clone git@github.com:ibebrett/arachnid.git $USER_HOME/arachnid
+git clone https://github.com/ibebrett/arachnid.git $USER_HOME/arachnid
+sudo pip install $USER_HOME/arachnid
 
 # create environment file
 cat <<EOF > $USER_HOME/env.sh
-export PATH=$PATH:/home/ubuntu/phantom/bin
+export PATH=\$PATH:/home/ubuntu/phantom/bin
 EOF
